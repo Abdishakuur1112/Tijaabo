40ad04209fe19d9722d65f32273bb019145b873d
Abdishakuur1112 <eng.abdishakuur2022@gmail.com>
2024-03-06 09:34:33 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <div>
+  <h1 className='text-5xl'>Welcom</h1>
+ </div>
+)
